74dd7af3f49aec082d348ef11badf8987914d487
kwatras <01168848@pw.edu.pl>
2022-01-16 23:42:03 +0100

    changes to sim step calculations

--- movement_simulation.h
+++ movement_simulation.h
@@ -3,7 +3,6 @@
 
 #include "core_types.h"
 #include "force_simulation.h"
-#include <cmath>
 
 
 /// Changes CelestialBody properties over each sim step
@@ -13,10 +12,10 @@
 /// float dt: time step of the simulation
 void sim_step(Vector3& gravitational_force, CelestialBody& body, float dt)
 {
-	Vector3 acceleration = gravitational_force* (1/body.mass); //acceleration of a body
+	Vector3 acceleration = gravitational_force * (1/body.mass); //acceleration of a body
 
-    body.velocity += acceleration * dt; //velocity of a body;
-    body.position += body.velocity * dt + acceleration * 0.5 * pow(dt, 2); //position of a body
+    body.velocity += acceleration * dt; //velocity of a body
+    body.position += body.velocity * dt + acceleration * ((dt * dt) / 2); //position of a body
 }
 
 #endif //SOLARSYSTEMSIM_MOVEMENT_SIMULATION_H_
