7590e3a63a0bf5b237bd14ee9e350543f09ccd70
kwatras <01168848@pw.edu.pl>
2022-02-08 18:08:21 +0100

    less vector multiplication in force calculations

--- movement_simulation.h
+++ movement_simulation.h
@@ -13,10 +13,10 @@
 template<typename T>
 void sim_step(Vector3<T>& gravitational_force, CelestialBody<T>& body, float dt)
 {
-	Vector3<T> acceleration = gravitational_force * (1/body.mass); //acceleration of a body
+    Vector3<T> acceleration_times_dt = gravitational_force * (dt/body.mass); //acceleration of a body
 
-    body.velocity += acceleration * dt; //velocity of a body
-    body.position += body.velocity * dt + acceleration * ((dt * dt) / 2); //position of a body
+    body.velocity += acceleration_times_dt; //velocity of a body
+    body.position += body.velocity * dt + acceleration_times_dt * (dt / 2); //position of a body
 }
 
 #endif //SOLARSYSTEMSIM_MOVEMENT_SIMULATION_H_
